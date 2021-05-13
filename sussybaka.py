455a296f8800b95a41dbcb10a960b76436a222f9
n-betayev <n.betayev@gmail.com>
2021-05-13 17:52:51 -0400

    added quick ai chat bot

--- sussybaka.py
+++ sussybaka.py
@@ -10,6 +10,7 @@ import asyncio
 #for multiple commands
 import random
 import time
+import cleverbotfree
 
 #for bot functionality
 import os
@@ -30,10 +31,10 @@ bot = Bot(command_prefix='baka ')
 #bot info v.0.1
 @bot.command(name='info',
              description="gives ur lil sussy baka ass the information",
-             help = "gives ur lil sussy baka ass the information"
+             help="gives ur lil sussy baka ass the information"
              )
 async def getInfo(context):
-    await context.send('sussy baka bot v. 0.1.1\ni love you')
+    await context.send('sussy baka bot v. 0.2.0\ni love you')
 
 #bot yes/no v.0.1
 @bot.command(name='yes/no',
@@ -42,11 +43,45 @@ async def getInfo(context):
              aliases=['y/n', '8ball']
              )
 async def ynQuestion(context):
-    f = open("./ynqResponse.txt","r")
+    f = open("./etc/ynqResponse.txt","r")
     responses = f.read().split("','")
     f.close()
     await context.send(context.message.author.mention + " " + random.choice(responses))
 
+#bot talk
+@bot.command(name='conversation',
+             description="have the bot talk to you",
+             brief="have the bot talk to your depressed, lonely, female-deprived life",
+             aliases=['talk', 'imlonely', 'chat']
+             )
+@commands.cooldown(1,120,commands.BucketType.default)
+async def chat(context):           
+    async with cleverbotfree.async_playwright() as p_w:
+        c_b = await cleverbotfree.CleverbotAsync(p_w)
+        await context.send("hello baka")
+        while True:
+            try:
+                msg = await bot.wait_for('message', check=lambda message: message.author == context.author, timeout=30) # 30 seconds to reply
+                user_input = msg.content
+            except:
+                await context.send("reply faster baka, i'm leaving")
+                break
+            
+            if user_input == 'quit':
+                await context.send("fuck you too, baka.")
+                break
+            
+            response = await c_b.single_exchange(user_input)
+            try:
+                await context.send(response)
+            except:
+                await context.send("i didn't understand what you said, baka. try again.")
+        await c_b.browser.close()
+@chat.error
+async def chat_error(context,error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await context.send('I\'m have recently had a conversation... ask me to talk again in {:.2f}s.'.format(error.retry_after))
+        
 #bot say v.0.1
 #bot says something. soundboard ig?
 @bot.command(name='say',
@@ -63,16 +98,13 @@ async def botSay(context, arg):
     
     sound = arg.lower()
     source = f"./audio/{sound}.mp3"
-    if not path.exists(source):
-        await context.send("i can't say " + arg + " yet, baka.")
-        return
-    
     vc = await cn.connect()
     vc.play(discord.FFmpegPCMAudio(executable="C:/FFmpeg/bin/ffmpeg.exe", source=source))
     while vc.is_playing():
        await asyncio.sleep(1)
     vc.stop()
     await vc.disconnect()
+    
 @botSay.error
 async def botSay_error(context,error):
     if isinstance(error, commands.CommandOnCooldown):
@@ -85,7 +117,7 @@ async def botSay_error(context,error):
              help = "simulates a genshin roll you gacha-addicted mongrel",
              aliases=['gRoll','genshin']
              )
-@commands.cooldown(1, 30, commands.BucketType.user)
+@commands.cooldown(1, 5, commands.BucketType.user)
 async def genshinRoll(context, arg):
     if arg == "char" or arg == "character" or arg == "c":
         probTable = [6, 26, 25, 943]
@@ -97,7 +129,7 @@ async def genshinRoll(context, arg):
             responses = f.read().split(",")
             f.close()
             
-            await context.send("Congratulations, degenerate. You got a " + random.choice(responses) + ".")
+            await context.send("Congratulations, " + context.message.author.mention + ". You got a " + random.choice(responses) + ".")
     elif arg == "weapon" or arg == "w":
         probTable = [7, 30, 30, 933]
         rollTable = ["5starW", "4starC", "4starW", "3star"]
@@ -108,10 +140,10 @@ async def genshinRoll(context, arg):
             responses = f.read().split(",")
             f.close()
             
-            await context.send("Congratulations, degenerate. You got a " + random.choice(responses) + ".")
+            await context.send("Congratulations, " + context.message.author.mention + ". You got a " + random.choice(responses) + ".")
     else:
         await context.send("you little baka, " + arg + " is not a valid roll type. type in baka gRoll c or something u little baka.")
-@genshinRoll.error
+@genshinRoll.error #prevent command spam
 async def genshinRoll_error(context,error):
     if isinstance(error, commands.CommandOnCooldown):
         await context.send('Baka... you can\'t roll again for {:.2f}s.'.format(error.retry_after))
@@ -138,9 +170,7 @@ async def stockPrice(context, arg):
 
         table.add_row(body)
         
-        await context.send("here's the price of " + arg + " stock my little pogchamp")
-        await context.send("retrieval time: " + str(data.index[0]))
-        await context.send("`" + table.get_string() + "`")
+        await context.send("here's the price of " + arg + " stock my little pogchamp\nretrieval time: " + str(data.index[0]) + "\n`" + table.get_string() + "`")
 
 
 
@@ -169,5 +199,6 @@ async def on_message(message):
         await message.channel.send('Among us? Why don\'t you get among some bitches?')
 
     await bot.process_commands(message)
+
 
 bot.run(TOKEN)
